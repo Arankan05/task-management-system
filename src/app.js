636c9de4b8bdbd54e53eaf5e1a00c5073b5c0330
Arankan <arankan2003@gmail.com>
2026-06-03 07:59:00 +0530

    Completed remaining Phase 1 setup and Swagger docs

--- src/app.js
+++ src/app.js
@@ -16,3 +16,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+);
