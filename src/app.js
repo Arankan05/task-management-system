8f0fb90f6951ca1adbc37666ac56c60852da389c
Arankan <arankan2003@gmail.com>
2026-06-03 07:37:44 +0530

    Completed authentication system and backend architecture

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Task Management Backend Running");
@@ -16,3 +20,16 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+const errorMiddleware = require(
+  "./middleware/errorMiddleware"
+);
+
+app.use(errorMiddleware);
+
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+app.use(helmet());
+app.use(morgan("dev"));
---
+++ src/middleware/validateRegister.js
@@ -0,0 +1,31 @@
+const validator = require("validator");
+
+const validateRegister = (
+    req,
+    res,
+    next
+) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            message: "All fields are required",
+        });
+    }
+
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({
+            message: "Invalid email",
+        });
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({
+            message: "Password must be at least 6 characters",
+        });
+    }
+
+    next();
+};
+
+module.exports = validateRegister;
