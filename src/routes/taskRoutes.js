689cfa6a3f5fb6efd22c44c722962cff6cd475e7
AAKP03 <alokakariyawasam23@gmail.com>
2026-06-10 20:52:20 +0530

    feat:Add Task Assignment System

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -4,9 +4,18 @@ const taskController = require("../controllers/taskController");
 const authenticate = require("../middleware/authMiddleware");
 
 router.post("/", authenticate, taskController.createTask);
+
 router.get("/", authenticate, taskController.getAllTasks);
+router.get("/my-tasks", authenticate, taskController.getMyTasks);
+router.get("/assigned/:userId", authenticate, taskController.getTasksByAssignee);
 router.get("/:id", authenticate, taskController.getTaskById);
+
 router.put("/:id", authenticate, taskController.updateTask);
+
 router.delete("/:id", authenticate, taskController.deleteTask);
+
+router.patch("/:id/assign", authenticate, taskController.assignTask);
+router.patch("/:id/unassign", authenticate, taskController.unassignTask);
+
 
 module.exports = router;
