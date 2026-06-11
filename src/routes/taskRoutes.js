a013b91cfaac6e6c5d2997863cca69cbcbe059d9
AAKP03 <alokakariyawasam23@gmail.com>
2026-06-12 00:03:25 +0530

    feat: Add Priority & Status Management with filtering

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/taskController");
 const authenticate = require("../middleware/authMiddleware");
+const { task } = require("../config/db");
 
 router.post("/", authenticate, taskController.createTask);
 
 router.get("/", authenticate, taskController.getAllTasks);
 router.get("/my-tasks", authenticate, taskController.getMyTasks);
+router.get("/filter", authenticate, taskController.getTasksByFilter);
 router.get("/assigned/:userId", authenticate, taskController.getTasksByAssignee);
 router.get("/:id", authenticate, taskController.getTaskById);
 
@@ -16,6 +18,7 @@ router.delete("/:id", authenticate, taskController.deleteTask);
 
 router.patch("/:id/assign", authenticate, taskController.assignTask);
 router.patch("/:id/unassign", authenticate, taskController.unassignTask);
-
+router.patch("/:id/status", authenticate, taskController.updateTaskStatus);
+router.patch("/:id/priority", authenticate, taskController.updateTaskPriority);
 
 module.exports = router;
