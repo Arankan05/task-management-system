7a21d6ee49f765a6cbb405bf44c5ce71678c3c31
AAKP03 <alokakariyawasam23@gmail.com>
2026-06-12 09:48:53 +0530

    feat: Add validation middleware to routes and role-based access control

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/taskController");
 const authenticate = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 const { validateCreateTask, validateUpdateTask } = require("../middleware/taskValidation");
 
-router.post("/", authenticate, taskController.createTask);
+router.post("/", authenticate, validateCreateTask, taskController.createTask);
 
 router.get("/", authenticate, taskController.getAllTasks);
 router.get("/my-tasks", authenticate, taskController.getMyTasks);
@@ -12,12 +13,12 @@ router.get("/filter", authenticate, taskController.getTasksByFilter);
 router.get("/assigned/:userId", authenticate, taskController.getTasksByAssignee);
 router.get("/:id", authenticate, taskController.getTaskById);
 
-router.put("/:id", authenticate, taskController.updateTask);
+router.put("/:id", authenticate,validateUpdateTask, taskController.updateTask);
 
-router.delete("/:id", authenticate, taskController.deleteTask);
+router.delete("/:id", authenticate, roleMiddleware("ADMIN"), taskController.deleteTask);
 
-router.patch("/:id/assign", authenticate, taskController.assignTask);
-router.patch("/:id/unassign", authenticate, taskController.unassignTask);
+router.patch("/:id/assign", authenticate, roleMiddleware("ADMIN","COLLABORATOR"), taskController.assignTask);
+router.patch("/:id/unassign", authenticate, roleMiddleware("ADMIN","COLLABORATOR"), taskController.unassignTask);
 router.patch("/:id/status", authenticate, taskController.updateTaskStatus);
 router.patch("/:id/priority", authenticate, taskController.updateTaskPriority);
 
